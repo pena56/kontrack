8a1af4cd23c3150dddeb0f76f7756f6984c5aceb
Moses <ogbopinamoses@gmail.com>
2025-09-09 16:42:15 +0100

    chore: setup backend for admin

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@workspace/ui/components/button";
+"use client";
+
+import { api } from "@workspace/backend/convex/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Page() {
+  const allNotes = useQuery(api.notes.getNotes);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Admin</h1>
-        <Button size="sm">Button</Button>
+        {allNotes?.map((note) => (
+          <div key={note._id}>
+            <p>{note.title}</p>
+            <p>{note.body}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
